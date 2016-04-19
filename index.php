b2066a415c615e3c71d46b474b261867ed73e95f
kevinvennitti <kevin.vennitti@gmail.com>
2016-04-19 11:51:34 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8" />
+    <title>Logotouch</title>
+    
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    
+    
+    <script src="lib/jquery-1.12.2.min.js"></script>
+    <script src="lib/jquery-ui.min.js"></script>
+    <script src="lib/jquery.ui.touch-punch.min.js"></script>
+    <script src="lib/hammer.min.js"></script>
+    <script src="lib/jquery.hammer.js"></script>
+    <script src="js/script.js"></script>
+</head>
+<body>
+  
+  <div id="StickersContainer">
+  </div>
+  
+  <a style="position:fixed;bottom:0;right:0;width:20px;height:20px;background:#fff;display:block;" onclick="window.location.reload();"></a>
+  
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,223 @@
+var thresholdSwipeX = 120; // Tense
+var thresholdSwipeY = 60; // Person
+var thresholdSwipeScale = 1; // Zoom
+
+var SCENARIO = 'default';
+var WORDS;
+var STICKERS;
+
+var touch = {
+  x: 0,
+  y: 0
+};
+
+/*
+ *  STICKERS[index].prop = val  Change value
+ *  attr('data-prop',val)       Fetch CSS selector [data-prop="val"]
+ */
+
+$(function() {
+  
+  $.getJSON( "scenarios/"+SCENARIO+".json", function (scenario) {
+    WORDS = scenario.words;
+    STICKERS = scenario.stickers;
+    
+    for(var index in STICKERS) { 
+      console.log(STICKERS[index]); 
+      
+      var DOM_Sticker = $('<div/>')
+        .addClass('Sticker Type__'+STICKERS[index].type)
+        .prop('id','Sticker'+index)
+        .attr('data-index',index)        
+        .attr('data-tense',STICKERS[index].tense)
+        .attr('data-person',STICKERS[index].person)
+        .attr('data-zoom',STICKERS[index].zoom);
+        
+      var DOM_StickerTuile = $('<div/>').addClass('Sticker__tuile').appendTo(DOM_Sticker);
+      var DOM_StickerInner = $('<div/>').addClass('Sticker__inner').appendTo(DOM_Sticker);
+      
+      DOM_Sticker.appendTo($('#StickersContainer'));
+      
+      initStickerEvents(index);
+    }
+  }).done(function(){ 
+  });
+  
+  
+  function initStickerEvents(index) {
+      
+    var sticker = document.getElementById('Sticker'+index);
+    var $sticker = $(sticker);
+    var $inner = $sticker.find('.Sticker__inner');
+    var $tuile = $sticker.find('.Sticker__tuile');
+    
+    var isPressed = false;
+    var triggeredRotate = false;
+    
+    $sticker.draggable({
+      containment: $('#StickersContainer'),
+      
+      drag: function(event,ui){
+        if (isPressed) return false;
+      }
+      
+    });
+    
+    $sticker.css({
+      'left':Math.random()*(window.innerWidth-$sticker.width()*2),
+      'top':Math.random()*(window.innerHeight-$sticker.width()*2)  
+    });
+    
+    // Init vars:
+    
+    var word = '';
+    
+    changeState();
+    
+    // Set gestures:
+  
+    var mc = new Hammer.Manager(sticker);
+    
+    mc.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) );
+    mc.add( new Hammer.Tap({ event: 'singletap' }) );
+    mc.add( new Hammer.Pinch({ event: 'pinch', pointers: 2, threshold: 0 }) );
+    mc.add( new Hammer.Pan({ event: 'drag', pointers: 1 }) );
+    mc.add( new Hammer.Pan({ event: 'swipex', pointers: 1, direction: 6, threshold: 4 }) );
+    mc.add( new Hammer.Pan({ event: 'swipey', pointers: 1, direction: 24, threshold: 4 }) );
+    mc.add( new Hammer.Press({ event: 'press', pointers: 1, time: 200, threshold: 99999999999999999999999 }) );
+    
+    mc.get('doubletap').recognizeWith('singletap');
+    mc.get('singletap').requireFailure('doubletap');
+    mc.get('pinch').set({ enable: true });
+    
+    mc.on('press', function(ev) {
+      isPressed = true;
+      $sticker.addClass('isPressed');
+    });
+    
+    mc.on('pressup panup pancancel pinchend touchend release mouseup', function(ev) {
+      releasePress();
+    });
+    
+    mc.on('swipex', function(ev) {      
+      if (isPressed) {        
+        var oldStepX = $sticker.data('oldStepX');
+        var stepX = parseInt(ev.distance/thresholdSwipeX);
+        
+        $sticker.data('oldStepX', stepX);
+        $sticker.data('distance',ev.distance);
+        
+        // We change the value when the gesture fetches thresholdSwipeX's step
+        
+        if (stepX != oldStepX) {
+          if (ev.direction == Hammer.DIRECTION_LEFT) {
+            STICKERS[index].tense--;
+          }
+          if (ev.direction == Hammer.DIRECTION_RIGHT) {
+            STICKERS[index].tense++;
+          }
+        }
+            
+        STICKERS[index].tense = Math.min(Math.max(parseInt(STICKERS[index].tense), 0), WORDS[STICKERS[index].zooms[STICKERS[index].zoom]].tenses.length-1);
+              
+        console.log('Change: Tense');
+        
+        changeState(index);
+        
+        if (ev.isFinal) {
+          releasePress();
+        }
+      }
+    });  
+    mc.on('swipey', function(ev) {     
+      if (isPressed) {
+        var oldStepY = $sticker.data('oldStepY');
+        var stepY = parseInt(ev.distance/thresholdSwipeY);
+        
+        $sticker.data('oldStepY', stepY);
+        $sticker.data('distance',ev.distance);
+        
+        
+        // We change the value when the gesture fetches thresholdSwipeY's step
+        
+        if (stepY != oldStepY) {
+          if (ev.direction == Hammer.DIRECTION_UP) {
+            STICKERS[index].person--;
+          }
+          if (ev.direction == Hammer.DIRECTION_DOWN) {
+            STICKERS[index].person++;
+          }
+        }
+        
+        STICKERS[index].person = Math.min(Math.max(parseInt(STICKERS[index].person), 0), WORDS[STICKERS[index].zooms[STICKERS[index].zoom]].tenses[STICKERS[index].tense].length-1);
+        
+        console.log('Change: Person');
+        
+        changeState(index);  
+        
+        if (ev.isFinal) {
+          releasePress();
+        }
+      }    
+    });  
+    
+    mc.on('pinchstart', function(ev) {
+      $sticker.data('scaleTemp',STICKERS[index].zoom);
+      $sticker.attr('data-scaleTemp',STICKERS[index].zoom);
+    });
+    
+    mc.on('pinch', function(ev) {
+      
+      var scaleTemp = $sticker.data('scaleTemp');
+      var scalePinch = (ev.scale/thresholdSwipeScale);
+      var scaleGot;
+      
+      if (scalePinch >= 1) {
+        scaleGot = parseInt(Math.floor(ev.scale-1));
+      }
+      
+      if (scalePinch < 1 && scalePinch >= .8) {
+        scaleGot = -1;
+      }
+      
+      if (scalePinch < .8 && scalePinch >= .6) {
+        scaleGot = -2;
+      }
+      
+      if (scalePinch < .6) {
+        scaleGot = -3;
+      }
+      scaleTemp = scaleTemp+scaleGot;
+      scaleTemp = Math.min(Math.max(scaleTemp, 0), STICKERS[index].zooms.length-1);
+      
+      STICKERS[index].zoom = scaleTemp;
+      
+    //  console.log('Change: zoom');
+      
+      changeState(index);
+      
+      console.log(ev.rotation);
+      
+      if (!triggeredRotate && (ev.rotation > 160 && ev.rotation < 200) || (ev.rotation < -160 && ev.rotation > -200)) {
+        triggeredRotate = true;
+        
+        console.log('Set antonym');
+      }
+    });
+    
+    function releasePress() {
+      isPressed = false;
+      triggeredRotate = false;
+      $sticker.removeClass('isPressed');
+    }
+    
+    function changeState() {    
+      $sticker.attr('data-zoom', STICKERS[index].zoom);
+      $sticker.attr('data-person', STICKERS[index].person);
+      $sticker.attr('data-tense', STICKERS[index].tense);
+      
+      word = WORDS[STICKERS[index].zooms[STICKERS[index].zoom]].tenses[STICKERS[index].tense][STICKERS[index].person];
+      $inner.text(word);
+    }
+  }
+});
